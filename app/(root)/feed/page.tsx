b5b0c7a04a3ccd62fe32bbedcf10b4cedcb5545b
Al Gibran <alg405544@gmail.com>
2026-01-01 19:45:36 +0700

    added and organized feed page

--- app/(root)/feed/page.tsx
+++ app/(root)/feed/page.tsx
@@ -15,6 +15,7 @@ import { getFeedPosts, LatestPost } from "@/lib/posts"
 import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import { getAvatarColor, getInitial } from "@/lib/utils"
 import { LikeButton } from "@/components/like-button"
+import { playfair } from "@/fonts"
 
 const Page = () => {
   const [posts, setPosts] = useState<LatestPost[]>([])
@@ -33,48 +34,51 @@ const Page = () => {
   if (loading) return <p>Loading...</p>
 
   return (
-    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-      {posts.map((post) => (
-        <Card key={post.id}>
-          <CardHeader>
-            <CardTitle>{post.title}</CardTitle>
-            <CardDescription>
-              by {post.author?.name ?? "Unknown"}
-            </CardDescription>
+    <>
+      <h1 className={`${playfair.className} text-5xl mb-3`}>Feed</h1>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 ml-3">
+        {posts.map((post) => (
+          <Card key={post.id}>
+            <CardHeader>
+              <CardTitle>{post.title}</CardTitle>
+              <CardDescription>
+                by {post.author?.name ?? "Unknown"}
+              </CardDescription>
+  
+              <CardAction>
+                <Avatar>
+                  <AvatarFallback
+                    className={`${getAvatarColor(post.author?.name)} text-white font-bold`}
+                  >
+                    {getInitial(post.author?.name)}
+                  </AvatarFallback>
+                </Avatar>
+              </CardAction>
+            </CardHeader>
 
-            <CardAction>
-              <Avatar>
-                <AvatarFallback
-                  className={`${getAvatarColor(post.author?.name)} text-white font-bold`}
-                >
-                  {getInitial(post.author?.name)}
-                </AvatarFallback>
-              </Avatar>
-            </CardAction>
-          </CardHeader>
-
-          <CardContent className="break-words whitespace-pre-wrap lg:max-h-40 lg:overflow-y-auto">
-            {post.content}
-          </CardContent>
-
-          <CardFooter className="flex items-center justify-between">
-            <p className="text-sm text-muted-foreground">
-              {new Date(post.created_at).toLocaleDateString("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "numeric",
-              })}
-            </p>
-
-            <LikeButton
-              postId={post.id}
-              initialLiked={(post.user_like?.length ?? 0) > 0}
-              initialCount={post.likes?.[0]?.count ?? 0}
-            />
-          </CardFooter>
+            <CardContent className="break-words whitespace-pre-wrap lg:max-h-40 lg:overflow-y-auto">
+              {post.content}
+            </CardContent>
+  
+            <CardFooter className="flex items-center justify-between">
+              <p className="text-sm text-muted-foreground">
+                {new Date(post.created_at).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
+                })}
+              </p>
+  
+              <LikeButton
+                postId={post.id}
+                initialLiked={(post.user_like?.length ?? 0) > 0}
+                initialCount={post.likes?.[0]?.count ?? 0}
+              />
+            </CardFooter>
         </Card>
       ))}
     </div>
+    </>
   )
 }
 
